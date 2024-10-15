1e5f04709eb519dd299649e409298ca659838709
rafa-canseco <rafa.canseco@gmail.com>
2024-10-15 14:55:26 -0600

    add vercel deployment link

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,14 +20,14 @@ function Home() {
   }
 
   if (authenticated && isRegistered) {
-    return null; // Esto evita un flash del contenido de Home antes de la redirección
+    return null;
   }
 
   return (
     <div className="w-full flex items-center justify-center min-h-screen">
       <div className="mx-auto grid w-[350px] gap-6">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-40">
-          CompraEnAmazonConCrypto
+          CompraEnAmazonConCrypto test
         </h1>
         <p className="leading-7 [&:not(:first-child)]:mt-6 text-balance text-muted-foreground mb-40">
           Self explanatory
